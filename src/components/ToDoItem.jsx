59bf53e76a0eba04e4bbcbe5042b42d9c4a8b3cb
sana2990 <sanaasad2990@gmail.com>
2026-05-05 17:30:56 +0530

    components added

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,18 @@
+function ToDoItem(props){
+    return(
+        <tr>
+      <td>{props.items.item}</td>
+      <td>
+        <input
+          type="checkbox"
+          checked={props.items.isCompleted}
+        />
+      </td>
+      <td>
+        <button>Delete</button>
+      </td>
+    </tr>
+    )
+}
+
+export default ToDoItem;
